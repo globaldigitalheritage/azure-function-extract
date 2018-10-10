15e50ecb2ddd1073e7e665cd12e0477fcf36d115
Vincent Meijer <meijer.vincent@gmail.com>
2018-10-10 11:40:51 +0200

    #67 Move StorageHandler to separate file

--- ExtractArchiveBlob.cs
+++ ExtractArchiveBlob.cs
@@ -1,16 +1,10 @@
-using System;
+
+using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.Azure.WebJobs.Extensions.SendGrid;
 using SendGrid.Helpers.Mail;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace GDH.ExtractArchiveBlob
 {
@@ -30,8 +24,8 @@ namespace GDH.ExtractArchiveBlob
             await SendEmail($"Extracted: {name}.zip", storageHandler.GetEmailReport(), messageCollector, logger);
         }
 
-        private static async Task SendEmail(string subject, IList<string> body, IAsyncCollector<SendGridMessage> messageCollector, Logger logger)
-        {           
+        private static async Task SendEmail(string subject, List<string> body, IAsyncCollector<SendGridMessage> messageCollector, Logger logger)
+        {
             var emailToAddresses = System.Environment.GetEnvironmentVariable("EmailTo");
             var emailFromAddress = System.Environment.GetEnvironmentVariable("EmailFrom");
 
@@ -55,86 +49,6 @@ namespace GDH.ExtractArchiveBlob
 
                 await messageCollector.AddAsync(message);
             }
-        }
-    }
-
-    public class StorageHandler
-    {
-        private Logger _logger;
-
-        public StorageHandler(Logger logger)
-        {
-            _logger = logger;
-        }
-
-        public async Task ExtractAndUpload(Stream zipStream, string archiveName)
-        {
-            CloudStorageAccount storageAccount = GetStorageAccount();
-            string containerName = GetContainerName();
-            string outputFolder = GetOutputFolder(archiveName);
-
-            var blobClient = storageAccount.CreateCloudBlobClient();
-            var container = blobClient.GetContainerReference(containerName);
-
-            await UploadZipContents(zipStream, outputFolder, container, archiveName);
-
-        }
-
-        private CloudStorageAccount GetStorageAccount()
-        {
-            var storageAccountConnectionString = System.Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            var storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
-            _logger.LogInformation($"Using storage account: {storageAccount.ToString()}");
-            return storageAccount;
-        }
-
-        private string GetContainerName()
-        {
-            var containerName = System.Environment.GetEnvironmentVariable("OutputContainerName");
-            _logger.LogInformation($"Output container: {containerName}");
-            return containerName;
-        }
-
-        private string GetOutputFolder(string name)
-        {
-            var outputPrefix = System.Environment.GetEnvironmentVariable("OutputPrefix");
-            var outputFolder = $"{outputPrefix}/{name}.zip_extracted";
-            _logger.LogInformation($"Output folder: {outputFolder}");
-            return outputFolder;
-        }
-
-        private async Task UploadZipContents(Stream zipStream, string outputFolder, CloudBlobContainer container, string archiveName)
-        {
-            _logger.LogInformation("Processing archive" +
-                                   $"\n Name:{archiveName} \n Size: {zipStream.Length} Bytes");
-
-            using (ZipArchive zip = new ZipArchive(zipStream))
-            {
-                _logger.LogInformation($"Total files in archive: {zip.Entries.Count}");
-
-                foreach (var entry in zip.Entries)
-                {
-                    _logger.LogTrace($"Processing: {entry.FullName}");
-
-                    var blobPath = $"{outputFolder}/{entry.FullName}";
-
-                    using (var entryStream = entry.Open())
-                    {
-                        if (entry.Length > 0)
-                        {
-                            _logger.LogTrace($"Uploading: {blobPath}");
-                            var blob = container.GetBlockBlobReference(blobPath);
-                            await blob.UploadFromStreamAsync(entryStream);
-                        }
-                    }
-                }
-            }
-            _logger.LogInformation($"Done processing blob: {archiveName}.zip");
-        }
-
-        public List<string> GetEmailReport()
-        {
-            return _logger.EmailReport;
         }
     }
 }
---
+++ StorageHandler.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace GDH.ExtractArchiveBlob
+{
+    public class StorageHandler
+    {
+        private Logger _logger;
+
+        public StorageHandler(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task ExtractAndUpload(Stream zipStream, string archiveName)
+        {
+            CloudStorageAccount storageAccount = GetStorageAccount();
+            string containerName = GetContainerName();
+            string outputFolder = GetOutputFolder(archiveName);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+
+            await UploadZipContents(zipStream, outputFolder, container, archiveName);
+
+        }
+
+        private CloudStorageAccount GetStorageAccount()
+        {
+            var storageAccountConnectionString = System.Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            var storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            _logger.LogInformation($"Using storage account: {storageAccount.ToString()}");
+            return storageAccount;
+        }
+
+        private string GetContainerName()
+        {
+            var containerName = System.Environment.GetEnvironmentVariable("OutputContainerName");
+            _logger.LogInformation($"Output container: {containerName}");
+            return containerName;
+        }
+
+        private string GetOutputFolder(string name)
+        {
+            var outputPrefix = System.Environment.GetEnvironmentVariable("OutputPrefix");
+            var outputFolder = $"{outputPrefix}/{name}.zip_extracted";
+            _logger.LogInformation($"Output folder: {outputFolder}");
+            return outputFolder;
+        }
+
+        private async Task UploadZipContents(Stream zipStream, string outputFolder, CloudBlobContainer container, string archiveName)
+        {
+            _logger.LogInformation("Processing archive" +
+                $"\n Name:{archiveName} \n Size: {zipStream.Length} Bytes");
+
+            using (ZipArchive zip = new ZipArchive(zipStream))
+            {
+                _logger.LogInformation($"Total files in archive: {zip.Entries.Count}");
+
+                foreach (var entry in zip.Entries)
+                {
+                    _logger.LogTrace($"Processing: {entry.FullName}");
+
+                    var blobPath = $"{outputFolder}/{entry.FullName}";
+
+                    using (var entryStream = entry.Open())
+                    {
+                        if (entry.Length > 0)
+                        {
+                            _logger.LogTrace($"Uploading: {blobPath}");
+                            var blob = container.GetBlockBlobReference(blobPath);
+                            await blob.UploadFromStreamAsync(entryStream);
+                        }
+                    }
+                }
+            }
+            _logger.LogInformation($"Done processing blob: {archiveName}.zip");
+        }
+
+        public List<string> GetEmailReport()
+        {
+            return _logger.EmailReport;
+        }
+    }
+}
